f0ba95b10dfe7a890578aa8e7fa8f3d4bd782422
jaewoong2 <jwisgenius@naver.com>
2020-08-08 18:56:38 +0900

    first commit

---
+++ src/templates/blog-spots.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import SEO from "../components/Seo";
+
+export default function BlogPost({ data }) {
+
+    const post = data.markdownRemark;
+
+  return (
+    <Layout>
+    <div>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+      excerpt
+    }
+  }
+`
